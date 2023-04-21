400b0745e335937fe8e4336dda495fe1a04805ac
Christopher Roussel <christopher@impleri.net>
2023-04-20 22:30:51 -0500

    fix: Update imports from PlayerSkills

--- common/src/main/java/net/impleri/dimensionskills/integrations/kubejs/events/RestrictionJS.java
+++ common/src/main/java/net/impleri/dimensionskills/integrations/kubejs/events/RestrictionJS.java
@@ -5,7 +5,7 @@ import dev.latvian.mods.rhino.util.HideFromJS;
 import net.impleri.dimensionskills.DimensionHelper;
 import net.impleri.dimensionskills.DimensionSkills;
 import net.impleri.dimensionskills.restrictions.Restriction;
-import net.impleri.playerskills.restrictions.AbstractRestrictionBuilder;
+import net.impleri.playerskills.integration.kubejs.api.AbstractRestrictionBuilder;
 import net.impleri.playerskills.utils.SkillResourceLocation;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
